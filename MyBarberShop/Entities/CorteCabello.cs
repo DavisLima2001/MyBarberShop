f23d13db3e244411f619d0d5f26e1fcc8ebbf6e6
DavisLima2001 <limalopezdavisnoe@gmail.com>
2026-04-25 09:30:14 -0600

    Creacion de nueva entidad.

---
+++ MyBarberShop/Entities/CorteCabello.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBarberShop.Entities
+{
+    public class CorteCabello
+    {
+        public int CorteCabelloId { get; set; }
+        public int Idcategory { get; set; }
+        public string Name { get; set; }
+        public string Description {  get; set; }
+        public int Price { get; set; }
+        public string ImageName { get; set; }
+    }
+}
