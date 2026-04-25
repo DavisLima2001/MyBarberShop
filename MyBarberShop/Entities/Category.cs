fd6eda8cd772277b3aa8bb6b7fffe5f0ac8686d1
DavisLima2001 <limalopezdavisnoe@gmail.com>
2026-04-25 08:41:31 -0600

    Add project files.

---
+++ MyBarberShop/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBarberShop.Entities
+{
+    public class Category
+    {
+        public int Idcategory { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ MyBarberShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyBarberShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
